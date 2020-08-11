667a508e4859aa0172ae587671189591243e8ffd
hillarioh <okerioh@gmail.com>
2020-08-11 15:29:49 +0300

    add calculator function and test

---
+++ src/calculator.js
@@ -0,0 +1,20 @@
+/* eslint-disable class-methods-use-this */
+class Calculator {
+  add(a, b) {
+    return a + b;
+  }
+
+  subtract(a, b) {
+    return a - b;
+  }
+
+  divide(a, b) {
+    return a / b;
+  }
+
+  multiply(a, b) {
+    return a * b;
+  }
+}
+
+module.exports = Calculator;
